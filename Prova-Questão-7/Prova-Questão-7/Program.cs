8407aed836f3728b355db00494ebc244f5c65836
Tayy76 <gimeneztata02@gmail.com>
2025-10-17 15:08:41 -0300

    Add files via upload

---
+++ Prova-Questão-7/Prova-Questão-7/Program.cs
@@ -0,0 +1,13 @@
+﻿int resultado, num1, num2;
+
+Console.WriteLine("Vamos fazer uma tabuada. Digite um numero inicial e um numero final.");
+
+num1 = int.Parse(Console.ReadLine());
+num2 = int.Parse(Console.ReadLine());
+
+
+for (int i = 1; i < num2; i++)
+{
+    resultado = num1 * i;
+    Console.WriteLine(resultado);
+}
