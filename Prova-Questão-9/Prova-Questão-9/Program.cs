59909af7bf1291c8ab8fb0cbb7404e166f483e43
Tayy76 <gimeneztata02@gmail.com>
2025-10-17 16:39:35 -0300

    Add files via upload

---
+++ Prova-Questão-9/Prova-Questão-9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+int sla = 0;
+string nome;
+
+string[] nomes = { "Taylan", "Agatha", "Mauricio", "Diego", "Calvin" };
+
+Console.WriteLine("Pesquise um nome.");
+
+nome = Console.ReadLine();
+
+for  (int i = 0; i < nomes.Length; i++)
+    if (sla == 0)
+    {
+        {
+            if (nome == nomes[i])
+            {
+                Console.WriteLine($"{nomes[i]} foi encontrado(a).");
+                sla = 1;
+            }
+            else if (nome != nomes[i] && i >= 4)
+            { Console.WriteLine("Esse usuário não foi encontrado(a)."); }
+
+        }
+    }
